1261f9ee6af559c1ce7575196ca21f7b8c1a3111
Prajapati Harsh <147126126+TonystarkM47@users.noreply.github.com>
2025-08-07 10:56:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//file upload middleware and package
+const fileupload = require('express-fileupload');
+//   the fileupload middleware is in 33-34 line
+
+var app = express();
+//mongoose connection
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/myinfo')
+.then(()=>{
+ console.log('Connected to MongoDB');
+})
+.catch(err => {
+ console.log("Error in DB Connection " +err);
+});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//fileupload middleware
+app.use(fileupload());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
